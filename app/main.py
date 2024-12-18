3e9c07936e06d3880df8eeaf12f97bbd6314797f
neeeksa <avgust.k31@gmail.com>
2024-12-19 01:17:49 +0200

    all tests have passed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+
+    with open(file_name, "w") as file:
+        while True:
+            text = input("Enter new line of content: ")
+            if text.lower() == "stop":
+                break
+            else:
+                file.write(text + "\n")
 
 
 if __name__ == "__main__":
